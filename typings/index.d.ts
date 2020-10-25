0397fd63b3193996e8add759123375c2743387ce
ducktrshessami <jwee761@gmail.com>
2020-10-25 16:07:45 -0700

    was that really necessary?

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,9 +1,7 @@
 declare module "scdl" {
     import {Readable} from "stream";
 
-    function scdl(URL: string): Readable;
-    export = scdl;
-
+    export function scdl(URL: string): Readable;
     export function setClientId(ID: string): void;
     export function setOauthToken(token: string): void;
     export function getInfo(URL: string): Promise<object>;
