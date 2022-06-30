5f8dec215b663e29d010bcb4b65ad92e6589d028
Josh Fish <jwee761@gmail.com>
2022-06-29 17:29:51 -0700

    url constants

---
+++ test/index.test.js
@@ -0,0 +1,24 @@
+const SONG_URL = "";
+const PLAYLIST_URL = "";
+
+describe("scdl", function () {
+    const URL = process.env.SONG_URL || SONG_URL;
+    if (URL) {
+
+    }
+    else {
+        console.warn("SONG_URL not found. Skipping scdl tests.\nSet the SONG_URL env or constant in the test script to run these tests.");
+        return;
+    }
+});
+
+describe("scdl.playlist", function () {
+    const URL = process.env.PLAYLIST_URL || PLAYLIST_URL;
+    if (URL) {
+
+    }
+    else {
+        console.warn("PLAYLIST_URL not found. Skipping scdl.playlist tests.\nSet the PLAYLIST_URL env or constant in the test script to run these tests.");
+        return;
+    }
+});
