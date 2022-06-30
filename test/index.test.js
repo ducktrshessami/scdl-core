832c2361a39bdc4ac00a4ab81389450dd69934a5
Josh Fish <jwee761@gmail.com>
2022-06-30 12:46:09 -0700

    auth test

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+const assert = require("assert");
+const scdl = require("../lib");
 
 const SONG_URL = "";
 const PLAYLIST_URL = "";
@@ -6,7 +8,14 @@ const PLAYLIST_URL = "";
 describe("scdl", function () {
     const URL = process.env.SONG_URL || SONG_URL;
     if (URL) {
-
+        it("sets/gets authorization properly", function () {
+            const foo = Math.random().toString();
+            const bar = Math.random().toString();
+            scdl.setClientID(foo);
+            scdl.setOauthToken(bar);
+            assert.strictEqual(scdl.clientID, foo);
+            assert.strictEqual(scdl.oauthToken, bar);
+        });
     }
     else {
         console.warn("SONG_URL not found. Skipping scdl tests.\nSet the SONG_URL env or constant in the test script to run these tests.");
