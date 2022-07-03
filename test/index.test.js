8e7cfaab5f21e0fbc9d673d4df898b0749e32a35
Josh Fish <jwee761@gmail.com>
2022-07-03 15:27:40 -0700

    getInfo test

--- test/index.test.js
+++ test/index.test.js
@@ -29,6 +29,10 @@ describe("scdl", function () {
             this.timeout(5000);
             assert(await scdl.awaitDownload(URL) instanceof Readable);
         });
+        it("scdl.getInfo resolves in object", async function () {
+            this.timeout(5000);
+            assert(typeof await scdl.getInfo(URL) === "object");
+        });
     }
     else {
         console.warn("SONG_URL not found. Skipping scdl tests.\nSet the SONG_URL env or constant in the test script to run these tests.");
