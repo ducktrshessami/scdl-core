5a2cc7fa4fc40c2fde7a0049674eb54fcd61f577
Josh Fish <jwee761@gmail.com>
2022-07-07 14:46:27 -0700

    track info properties

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,12 +2,79 @@ import { Readable } from "stream";
 
 declare module "scdl-core" {
     type StreamOptions = {
-        strict?: boolean,
-        preset?: string,
-        protocol?: string,
-        mimeType?: string,
+        strict?: boolean
+        preset?: string
+        protocol?: string
+        mimeType?: string
         quality?: string
     };
+
+    interface PublisherMetadata {
+
+    }
+
+    interface UserInfo {
+
+    }
+
+    interface TrackMedia {
+
+    }
+
+    interface TrackInfo {
+        artwork_url: string | null
+        caption: string | null
+        commentable: boolean
+        comment_count: number | null
+        created_at: string
+        description: string | null
+        downloadable: boolean
+        download_count: number | null
+        duration: number
+        full_duration: number
+        embeddable_by: string
+        genre: string | null
+        has_downloads_left: boolean
+        id: number
+        kind: string
+        label_name: string | null
+        last_modified: string
+        license: string
+        likes_count: number | null
+        permalink: string
+        permalink_url: string
+        playback_count: number | null
+        public: boolean
+        publisher_metadata: PublisherMetadata | null
+        purchase_title: string | null
+        purchase_url: string | null
+        release_date: string | null
+        reposts_count: number
+        secret_token: string | null
+        sharing: string
+        state: string
+        streamable: boolean
+        tag_list: string | null
+        title: string
+        track_format: string
+        uri: string
+        urn: string
+        user_id: number
+        visuals: any
+        waveform_url: string
+        display_date: string
+        media: TrackMedia
+        station_urn: string
+        station_permalink: string
+        track_authorization: string
+        monetization_model: string
+        policy: string
+        user: UserInfo
+    }
+
+    interface StreamableTrackInfo {
+
+    }
 
     const scdl: {
         (url: string, options?: StreamOptions): Readable;
@@ -18,9 +85,9 @@ declare module "scdl-core" {
         setClientID(id: string): void;
         setOauthToken(token: string): void;
         awaitDownload(url: string, options?: StreamOptions): Promise<Readable>;
-        getInfo(url: string): Promise<object>;
-        downloadFromInfo(info: object, options?: StreamOptions): Readable;
-        awaitDownloadFromInfo(info: object, options?: StreamOptions): Promise<Readable>;
+        getInfo(url: string): Promise<TrackInfo>;
+        downloadFromInfo(info: StreamableTrackInfo, options?: StreamOptions): Readable;
+        awaitDownloadFromInfo(info: StreamableTrackInfo, options?: StreamOptions): Promise<Readable>;
         validateURL(url: string): boolean;
         getPermalinkURL(url: string): string;
         playlist: {
