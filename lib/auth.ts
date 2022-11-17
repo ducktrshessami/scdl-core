a0354775e5dbb17b5454482bd0c3d7d22e2f9006
ducktrshessami <jwee761@gmail.com>
2022-11-16 17:26:56 -0800

    auth casing

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,10 @@
-export let client_id: string | null = null;
-export let oauth_token: string | null = null;
+export let clientID: string | null = null;
+export let oauthToken: string | null = null;
 
-export function setClientID(clientID: string | null): void {
-    client_id = clientID;
+export function setClientID(client_id: string | null): void {
+    clientID = client_id;
 }
 
 export function setOauthToken(token: string | null): void {
-    oauth_token = token;
+    oauthToken = token;
 }
