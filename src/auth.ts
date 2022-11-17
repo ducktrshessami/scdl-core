40591d670390cb55e253771bcab1022fe8bd146e
ducktrshessami <jwee761@gmail.com>
2022-11-17 14:19:01 -0800

    auth comments

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,18 @@
 export let clientID: string | null = null;
 export let oauthToken: string | null = null;
 
-export function setClientID(client_id: string | null): void {
-    clientID = client_id;
+/**
+ * Set the client_id to access the API with
+ */
+export function setClientID(id: string | null): void {
+    clientID = id;
 }
 
+/**
+ * Set the oauth_token to access the API with
+ * 
+ * This will be prioritized over a client_id
+ */
 export function setOauthToken(token: string | null): void {
     oauthToken = token;
 }
