67c847f2fcac15b22616cb19b539e014e20cea17
ducktrshessami <jwee761@gmail.com>
2022-11-17 15:01:52 -0800

    validatePlaylistURL regex

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -4,3 +4,10 @@
 export function validateURL(url: string): boolean {
     return /(?:^https?:\/\/)?(?:www.)?soundcloud\.com\/[\w-]+\/[\w-]+\/?(?:(?:\/?$)|(?:[?#]))/i.test(url);
 }
+
+/**
+ * Checks if a string matches the SoundCloud playlist URL format
+ */
+export function validatePlaylistURL(url: string): boolean {
+    return /(?:^https?:\/\/)?(?:www.)?soundcloud\.com\/[\w-]+\/sets\/[\w-]+(?:\/[\w-]+)?\/?(?:(?:\/?$)|(?:[?#]))/i.test(url);
+}
