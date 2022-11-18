62e8e6162957e798223d84dc0aefcc90c706470f
ducktrshessami <jwee761@gmail.com>
2022-11-17 23:17:03 -0800

    implement standard request

--- src/dispatch.ts
+++ src/dispatch.ts
@@ -1,4 +1,6 @@
 import { Dispatcher, getGlobalDispatcher } from "undici";
+import { ResponseData } from "undici/types/dispatcher";
+import { RequestError } from "./error";
 
 let dispatcher = getGlobalDispatcher();
 
@@ -14,4 +16,29 @@ export function setAgent(agent: Dispatcher): void {
  */
 export function getAgent(): Dispatcher {
     return dispatcher;
+}
+
+/**
+ * Create GET request options from a URL
+ */
+function createRequestOptions(url: string): Dispatcher.RequestOptions {
+    const parsedUrl = new URL(url);
+    return {
+        origin: parsedUrl.origin,
+        path: parsedUrl.pathname + parsedUrl.search,
+        method: "GET"
+    };
+}
+
+/**
+ * Perform a GET request
+ */
+export async function request(url: string): Promise<ResponseData> {
+    const res = await dispatcher.request(createRequestOptions(url));
+    if (res.statusCode < 400) {
+        return res;
+    }
+    else {
+        throw new RequestError(res.statusCode);
+    }
 }
