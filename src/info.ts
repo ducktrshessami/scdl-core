997faf32d5dd665c6f982536fbcd2cad424eb856
ducktrshessami <jwee761@gmail.com>
2022-11-18 14:51:28 -0800

    playlist types

--- src/info.ts
+++ src/info.ts
@@ -177,3 +177,43 @@ export type TrackInfo = {
     policy: string
     user: UserInfo
 };
+
+export type StreamablePlaylistInfo = {
+    tracks: Array<StreamableTrackInfo>
+};
+
+export type PlaylistInfo = {
+    artwork_url?: string
+    created_at: string
+    description?: string
+    duration: number
+    embeddable_by: string
+    genre?: string
+    id: number
+    kind: string
+    label_name?: string
+    last_modified: string
+    license: string
+    likes_count: number
+    managed_by_feeds: boolean
+    permalink: string
+    permalink_url: string
+    public: boolean
+    purchase_title?: string
+    purchase_url?: string
+    release_date?: string
+    reposts_count: number
+    secret_token?: string
+    sharing: string
+    tag_list?: string
+    title: string
+    uri: string
+    user_id: number
+    set_type?: string
+    is_album: boolean
+    published_at?: string
+    display_date: string
+    user: UserInfo
+    tracks: Array<TrackInfo>
+    track_count: number
+};
