dc2dba84573d3e51c19eba094d991e9cfd08be3b
ducktrshessami <jwee761@gmail.com>
2022-11-17 21:35:25 -0800

    comment playlist tests will rewrite later as playlist functionality is implemented

--- test/index.test.js
+++ test/index.test.js
@@ -73,58 +73,58 @@ describe("track", function () {
     }
 });
 
-describe("scdl.playlist", function () {
-    const URL = process.env.PLAYLIST_URL || PLAYLIST_URL;
-    if (URL) {
-        it("scdl.playlist.validateURL sync checks format", function () {
-            assert.strictEqual(scdl.playlist.validateURL(URL), true);
-            assert.strictEqual(scdl.validateURL("https://soundcloud.com/"), false);
-        });
-        it("scdl.playlist.getPermalinkURL always string", function () {
-            assert.strictEqual(typeof scdl.playlist.getPermalinkURL(URL), "string");
-            assert.strictEqual(typeof scdl.playlist.getPermalinkURL("foobar"), "string");
-        });
-        it("scdl.playlist resolves in readable? array", async function () {
-            this.timeout(10000);
-            const result = await scdl.playlist(URL);
-            assert.strictEqual(Array.isArray(result), true);
-            assert.strictEqual(result.every(item => item === null || item instanceof Readable), true);
-        });
-        it("scdl.playlist resolves in readable? array with hls protocol", async function () {
-            this.timeout(10000);
-            const result = await scdl.playlist(URL, { protocol: "hls" });
-            assert.strictEqual(Array.isArray(result), true);
-            assert.strictEqual(result.every(item => item === null || item instanceof Readable), true);
-        });
-        it("scdl.playlist readables have transcoding property", async function () {
-            this.timeout(10000);
-            const result = await scdl.playlist(URL);
-            assert.strictEqual(result.every(item => item === null || typeof item.transcoding === "object"), true);
-        });
-        it("scdl.playlist.getInfo resolves in object", async function () {
-            this.timeout(5000);
-            assert.strictEqual(typeof await scdl.playlist.getInfo(URL), "object");
-        });
-        describe("from info", function () {
-            let info;
-            before("fetching info", async function () {
-                info = await scdl.playlist.getInfo(URL);
-            });
-            it("scdl.playlist.downloadFromInfo resolves in readable? array", async function () {
-                this.timeout(10000);
-                const result = await scdl.playlist.downloadFromInfo(info);
-                assert.strictEqual(Array.isArray(result), true);
-                assert.strictEqual(result.every(item => item === null || item instanceof Readable), true);
-            });
-            it("scdl.playlist.downloadFromInfo readables have transcoding property", async function () {
-                this.timeout(10000);
-                const result = await scdl.playlist.downloadFromInfo(info);
-                assert.strictEqual(result.every(item => item === null || typeof item.transcoding === "object"), true);
-            });
-        });
-    }
-    else {
-        console.warn("PLAYLIST_URL not found. Skipping scdl.playlist tests.\nSet the PLAYLIST_URL env or constant in the test script to run these tests.");
-        return;
-    }
-});
+// describe("scdl.playlist", function () {
+//     const URL = process.env.PLAYLIST_URL || PLAYLIST_URL;
+//     if (URL) {
+//         it("scdl.playlist.validateURL sync checks format", function () {
+//             assert.strictEqual(scdl.playlist.validateURL(URL), true);
+//             assert.strictEqual(scdl.validateURL("https://soundcloud.com/"), false);
+//         });
+//         it("scdl.playlist.getPermalinkURL always string", function () {
+//             assert.strictEqual(typeof scdl.playlist.getPermalinkURL(URL), "string");
+//             assert.strictEqual(typeof scdl.playlist.getPermalinkURL("foobar"), "string");
+//         });
+//         it("scdl.playlist resolves in readable? array", async function () {
+//             this.timeout(10000);
+//             const result = await scdl.playlist(URL);
+//             assert.strictEqual(Array.isArray(result), true);
+//             assert.strictEqual(result.every(item => item === null || item instanceof Readable), true);
+//         });
+//         it("scdl.playlist resolves in readable? array with hls protocol", async function () {
+//             this.timeout(10000);
+//             const result = await scdl.playlist(URL, { protocol: "hls" });
+//             assert.strictEqual(Array.isArray(result), true);
+//             assert.strictEqual(result.every(item => item === null || item instanceof Readable), true);
+//         });
+//         it("scdl.playlist readables have transcoding property", async function () {
+//             this.timeout(10000);
+//             const result = await scdl.playlist(URL);
+//             assert.strictEqual(result.every(item => item === null || typeof item.transcoding === "object"), true);
+//         });
+//         it("scdl.playlist.getInfo resolves in object", async function () {
+//             this.timeout(5000);
+//             assert.strictEqual(typeof await scdl.playlist.getInfo(URL), "object");
+//         });
+//         describe("from info", function () {
+//             let info;
+//             before("fetching info", async function () {
+//                 info = await scdl.playlist.getInfo(URL);
+//             });
+//             it("scdl.playlist.downloadFromInfo resolves in readable? array", async function () {
+//                 this.timeout(10000);
+//                 const result = await scdl.playlist.downloadFromInfo(info);
+//                 assert.strictEqual(Array.isArray(result), true);
+//                 assert.strictEqual(result.every(item => item === null || item instanceof Readable), true);
+//             });
+//             it("scdl.playlist.downloadFromInfo readables have transcoding property", async function () {
+//                 this.timeout(10000);
+//                 const result = await scdl.playlist.downloadFromInfo(info);
+//                 assert.strictEqual(result.every(item => item === null || typeof item.transcoding === "object"), true);
+//             });
+//         });
+//     }
+//     else {
+//         console.warn("PLAYLIST_URL not found. Skipping scdl.playlist tests.\nSet the PLAYLIST_URL env or constant in the test script to run these tests.");
+//         return;
+//     }
+// });
