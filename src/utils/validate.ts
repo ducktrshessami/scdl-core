c4f9a26f43b9e70bda0482cfd2504c917505549b
ducktrshessami <jwee761@gmail.com>
2022-11-17 16:09:05 -0800

    update and export track url regex

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,8 +1,15 @@
 /**
+ * A regular expression that matches SoundCloud track URLs
+ * 
+ * Includes the `user` and `title` groups
+ */
+export const TrackURLPattern = /^(?:https?:\/\/)?(?:www.)?soundcloud\.com\/(?<user>[\w-]+)\/(?<title>[\w-]+)\/?(?=[?#]|$)/i;
+
+/**
  * Checks if a string matches the SoundCloud track URL format
  */
 export function validateURL(url: string): boolean {
-    return /(?:^https?:\/\/)?(?:www.)?soundcloud\.com\/[\w-]+\/[\w-]+\/?(?:(?:\/?$)|(?:[?#]))/i.test(url);
+    return TrackURLPattern.test(url);
 }
 
 /**
