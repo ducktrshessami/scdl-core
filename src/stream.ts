c705658e86db80e444fbddd4a53ab3026f3e7e5b
Josh Fish <jwee761@gmail.com>
2023-11-24 11:02:43 -0800

    refactor: emitter types

--- src/stream.ts
+++ src/stream.ts
@@ -260,12 +260,13 @@ type TranscodingStreamResponse = {
 
 interface Emitter<EventMap extends Record<string, any[]>> {
     emit<Event extends keyof EventMap>(event: Event, ...args: EventMap[Event]): boolean;
-    addListener<Event extends keyof EventMap>(event: Event, listener: (...args: EventMap[Event]) => void): this;
-    on<Event extends keyof EventMap>(event: Event, listener: (...args: EventMap[Event]) => void): this;
-    once<Event extends keyof EventMap>(event: Event, listener: (...args: EventMap[Event]) => void): this;
-    prependListener<Event extends keyof EventMap>(event: Event, listener: (...args: EventMap[Event]) => void): this;
-    prependOnceListener<Event extends keyof EventMap>(event: Event, listener: (...args: EventMap[Event]) => void): this;
-    removeListener<Event extends keyof EventMap>(event: Event, listener: (...args: EventMap[Event]) => void): this;
+    addListener<Event extends keyof EventMap>(event: Event, listener: (...args: EventMap[Event]) => any): this;
+    on<Event extends keyof EventMap>(event: Event, listener: (...args: EventMap[Event]) => any): this;
+    once<Event extends keyof EventMap>(event: Event, listener: (...args: EventMap[Event]) => any): this;
+    prependListener<Event extends keyof EventMap>(event: Event, listener: (...args: EventMap[Event]) => any): this;
+    prependOnceListener<Event extends keyof EventMap>(event: Event, listener: (...args: EventMap[Event]) => any): this;
+    removeListener<Event extends keyof EventMap>(event: Event, listener: (...args: EventMap[Event]) => any): this;
+    off(event: keyof EventMap, listener: (...args: any[]) => any): this;
 }
 
 type TranscodingStreamEvents = {
