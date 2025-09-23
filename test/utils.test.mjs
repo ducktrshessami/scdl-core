303191d33d92edae0042fd3bbcca84b7f809be61
ducktrshessami <cal.tf2.3@gmail.com>
2025-09-23 08:44:33 -0700

    test(utils): vitest assertion api

--- test/utils.test.mjs
+++ test/utils.test.mjs
@@ -1,5 +1,4 @@
-import assert from "assert";
-import { describe, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import * as scdl from "../dist/index.mjs";
 import { PLAYLIST_URL, TRACK_URL } from "./urls.mjs";
 
@@ -9,33 +8,34 @@ const playlistURL = process.env.PLAYLIST_URL || PLAYLIST_URL;
 describe.skipIf(!trackURL)("track", function () {
     test("TrackURLPattern matches groups properly", function () {
         const result = trackURL.match(scdl.TrackURLPattern);
-        assert(result);
-        assert.strictEqual(typeof result.groups?.user, "string");
-        assert.strictEqual(typeof result.groups?.title, "string");
+        expect(result).not.toBeNull();
+        expect(result.groups?.user).toBeTypeOf("string");
+        expect(result.groups?.title).toBeTypeOf("string");
     });
     test("validateURL sync checks format", function () {
-        assert.strictEqual(scdl.validateURL(trackURL), true);
-        assert.strictEqual(scdl.validateURL("https://soundcloud.com/"), false);
+        expect(scdl.validateURL(trackURL)).toBe(true);
+        expect(scdl.validateURL(trackURL)).toBe(true);
+        expect(scdl.validateURL("https://soundcloud.com/")).toBe(false);
     });
     test("getPermalinkURL always string", function () {
-        assert.strictEqual(typeof scdl.getPermalinkURL(trackURL), "string");
-        assert.strictEqual(typeof scdl.getPermalinkURL("foobar"), "string");
+        expect(scdl.getPermalinkURL(trackURL)).toBeTypeOf("string");
+        expect(scdl.getPermalinkURL("foobar")).toBeTypeOf("string");
     });
 });
 
 describe.skipIf(!playlistURL)("playlist", function () {
     test("PlaylistURLPattern matches groups properly", function () {
         const result = playlistURL.match(scdl.PlaylistURLPattern);
-        assert(result);
-        assert.strictEqual(typeof result.groups?.user, "string");
-        assert.strictEqual(typeof result.groups?.title, "string");
+        expect(result).not.toBeNull();
+        expect(result.groups?.user).toBeTypeOf("string");
+        expect(result.groups?.title).toBeTypeOf("string");
     });
     test("validatePlaylistURL sync checks format", function () {
-        assert.strictEqual(scdl.validatePlaylistURL(playlistURL), true);
-        assert.strictEqual(scdl.validatePlaylistURL("https://soundcloud.com/"), false);
+        expect(scdl.validatePlaylistURL(playlistURL)).toBe(true);
+        expect(scdl.validatePlaylistURL("https://soundcloud.com/")).toBe(false);
     });
     test("getPlaylistPermalinkURL always string", function () {
-        assert.strictEqual(typeof scdl.getPlaylistPermalinkURL(playlistURL), "string");
-        assert.strictEqual(typeof scdl.getPlaylistPermalinkURL("foobar"), "string");
+        expect(scdl.getPlaylistPermalinkURL(playlistURL)).toBeTypeOf("string");
+        expect(scdl.getPlaylistPermalinkURL("foobar")).toBeTypeOf("string");
     });
 });
